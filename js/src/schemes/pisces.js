f4593102dd28f530ebfa65d7409ee61043641a4e
ChrisKelaxon <ChrisKelaxon@163.com>
2017-03-03 23:07:14 +0800

    Site updated: 2017-03-03 23:07:08

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 02 2017 22:50:52 GMT+0800 (中国标准时间)
+// build time:Fri Mar 03 2017 23:06:53 GMT+0800 (中国标准时间)
 $(document).ready(function(){var e=$(".header-inner").height()+10;$("#sidebar").css({"margin-top":e}).show()});
 //rebuild by neat 
